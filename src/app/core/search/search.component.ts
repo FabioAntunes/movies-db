72cdde80fd1bc64f8e783e15ec57940a5d353e83
Fábio Antunes <fabioantuness@gmail.com>
2017-04-18 00:19:19 +0100

    Search input

--- src/app/core/search/search.component.ts
+++ src/app/core/search/search.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'mv-search',
-  templateUrl: './search.template.html'
+  templateUrl: './search.template.html',
+  styleUrls: ['./search.styles.scss']
 })
 export class SearchComponent implements OnInit {
   constructor() { }
